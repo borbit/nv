189fbefdd9006e82e7fdf89bb0fd03107b03e8b8
Serge Borbit <serge.borbit@gmail.com>
2013-01-24 21:30:17 +0200

    Config vars for the assets map and images map files. Comments.

--- bp/config.js
+++ bp/config.js
@@ -1,30 +1,35 @@
 /**
  * Module dependencies.
  */
- var path = require('path');
+var path = require('path');
 
 /**
  * Config
  */
 var cfg = module.exports;
 
-// Root directory path for the "static" staff (blocks, css, js).
+// Path to the root directory for the "static" staff (blocks, css, js).
 cfg.STATIC_ROOT = process.env.STATIC_ROOT || __dirname;
 
-// Root directory path for the "pages" staff (assets lists, layouts).
+// Path to the root directory for the "pages" staff (assets lists, layouts).
 cfg.PAGES_ROOT = process.env.PAGES_ROOT || path.join(__dirname, 'pages');
 
-// Root directory path for distributions builds.
+// Path to the root directory for distribution builds.
 cfg.DISTS_ROOT = process.env.DISTS_ROOT || path.join(__dirname, 'dists');
 
-// Root directory path for temporary directories used for distributions builds.
+// Path to the root directory for temporary directories used for distribution builds.
 cfg.TEMPS_ROOT = process.env.TEMPS_ROOT || '/tmp';
 
-// Path to the file contains the latest version of static distribution.
+// Path to the file contains version of the latest static distribution.
 cfg.VERSION_FILE = process.env.VERSION_FILE || path.join(cfg.DISTS_ROOT, 'version.json');
 
-// Path to the file contains the latest assets map of static distribution.
-cfg.MAP_FILE = process.env.MAP_FILE || path.join(cfg.DISTS_ROOT, 'map.json');
+// Path to the file contains the assets map of the latest static distribution.
+// If it is not specified than assets.json will be placed tot the dist directory.
+cfg.ASSETS_MAP_FILE = process.env.ASSETS_MAP_FILE || false;
 
-//Assets host url to be used for "background" images in the css build.
+// Path to the file contains the images map of the latest static distribution.
+// If it is not specified than images.json will be placed tot the dist directory.
+cfg.IMAGES_MAP_FILE = process.env.IMAGES_MAP_FILE || false;
+
+// Assets host url to be used for "background" images in the css build.
 cfg.ASSETS_HOSTS = process.env.ASSETS_HOSTS || '';
