fe794eca75f0390da09d42a12c0fc0db6d71279a
Serge Borbit <serge.borbit@gmail.com>
2013-07-03 14:47:32 +0300

    Made NV easely configurable. Made locales functionality optional

--- bp/config.js
+++ bp/config.js
@@ -9,12 +9,13 @@ c.TEMPLATE_ENGINE = process.env.TEMPLATE_ENGINE || '<%=engine%>';
 // nv-pack & production
 c.TEMPS_ROOT      = process.env.TEMPS_ROOT      || '/tmp';
 c.DISTS_ROOT      = process.env.DISTS_ROOT      || path.join(__dirname, 'dists');
-c.VERSION_FILE    = process.env.VERSION_FILE    || path.join(c.DISTS_ROOT, 'version.json');
+c.VERSION_FILE    = process.env.VERSION_FILE    || path.join(__dirname, 'dists', 'version.json');
 c.ASSETS_MAP_FILE = process.env.ASSETS_MAP_FILE || false;
 c.IMAGES_MAP_FILE = process.env.IMAGES_MAP_FILE || false;
 c.ASSETS_HOSTS    = process.env.ASSETS_HOSTS    || '';
 
-// locales
+// locales, off by default
+c.LOCALES         = false;
 c.LOCALE_DEFAULT  = process.env.LOCALE_DEFAULT || 'en';
 c.LOCALES_ROOT    = process.env.LOCALES_ROOT   || path.join(__dirname, 'i18n');
-c.LOCALES         = ['en']
+c.LOCALES_LIST    = ['en']
--- index.js
+++ index.js
@@ -1,5 +1,33 @@
+var nv = require('./lib/nv');
+
+function init(config) {
+  /**
+   * Extend default config
+   */
+  for (var i in config) {
+    nv.config[i] = config[i];
+  }
+
+  var c = nv.config;
+
+  /** 
+   * Setup i18n 
+   */
+  if (c.LOCALES) {
+    var i18n = require('i18n');
+    
+    i18n.configure({
+      locales       : c.LOCALES_LIST
+    , defaultLocale : c.LOCALE_DEFAULT
+    , directory     : c.LOCALES_ROOT
+    , indent        : '  ',
+    });
+  }
+}
+
 module.exports = {
-  server: require('./lib/server')
-, render: require('./lib/render')
-, watch: require('./lib/watch')
+  server : require('./lib/server')
+, render : require('./lib/render')
+, watch  : require('./lib/watch')
+, init   : init
 };
