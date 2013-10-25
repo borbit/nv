4fd527bf63419a0180e7f9e4e8ce98de730e5756
Serge Borbit <serge.borbit@gmail.com>
2013-10-25 17:54:24 +0300

    Cannot describe...

--- bp/config.js
+++ bp/config.js
@@ -7,13 +7,15 @@ c.STATIC_ROOT     = process.env.STATIC_ROOT     || __dirname;
 c.TEMPLATE_ENGINE = process.env.TEMPLATE_ENGINE || '<%=engine%>';
 c.ENV             = process.env.NODE_ENV        || 'development';
 
+// nv && production
+c.ASSETS_HOST     = process.env.ASSETS_HOST     || '';
+
 // nv-pack & production
 c.TEMPS_ROOT      = process.env.TEMPS_ROOT      || '/tmp';
 c.DISTS_ROOT      = process.env.DISTS_ROOT      || path.join(__dirname, 'dists');
 c.VERSION_FILE    = process.env.VERSION_FILE    || path.join(__dirname, 'dists', 'version.json');
 c.ASSETS_MAP_FILE = process.env.ASSETS_MAP_FILE || false;
 c.IMAGES_MAP_FILE = process.env.IMAGES_MAP_FILE || false;
-c.ASSETS_HOSTS    = process.env.ASSETS_HOSTS    || '';
 
 // locales, off by default
 c.LOCALES         = false;
